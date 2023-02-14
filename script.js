f62b1502681d82d4639b387cf4a33c73ad65dd98
Everton de A <everton.araujogta@gmail.com>
2023-02-14 20:57:40 -0300

    dfs;lkggjsdfgkl

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(document.getElementsById('nome'));
+const sub = document.getElementById('submit');
+
+sub.addEventListener('click', evento =>{
+
+    evento.preventDefault();
+
+    console.log(document.getElementById('nome').value)
+})
